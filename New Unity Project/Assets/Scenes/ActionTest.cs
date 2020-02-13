ce3c5a220e31953728bb21232b7856b4598b4596
Unknown <fj16315@my.bristol.ac.uk>
2020-02-13 18:40:56 +0000

    Recognises hands and button presses

---
+++ New Unity Project/Assets/Scenes/ActionTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+using Valve.VR;
+
+public class ActionTest : MonoBehaviour
+{
+  public SteamVR_Input_Sources handType; // 1 
+  public SteamVR_Action_Boolean teleportAction; // 2
+  public SteamVR_Action_Boolean grabAction; // 3 
+
+
+  public bool GetTeleportDown() // 1
+  {
+    return teleportAction.GetStateDown(handType);
+  }
+
+  public bool GetGrab() // 2
+  {
+    return grabAction.GetState(handType);
+  }
+
+void Update()
+  {
+    if(GetTeleportDown())
+    {
+      print("Teleport " + handType);
+    }
+
+    if(GetGrab())
+    {
+      print("Grab " + handType);
+    }
+  }
+}
